63f40cc7d23f6112225edfc48c4095cd602bd74d
Samkelwa <samkelwa101@gmail.com>
2024-02-29 23:29:36 +0200

    wrote the script for removing cart items using the 'remove' buttons, and the script to update the cart total when a cart item is removed. Also added some classes to the html elements tp facilitate the scripts and styled the classes for the same reason.

--- shopScript.js
+++ shopScript.js
@@ -1,2 +1,32 @@
 // Script for a shopping cart exercise I'm creating
 console.log("SHOPPING CART PROJECT!!!") ;
+
+
+
+const removeCartItemBtns = document.getElementsByClassName('remv-btn') ;
+console.log(removeCartItemBtns) ;
+for (i = 0; i < removeCartItemBtns.length; i++) {
+    const button = removeCartItemBtns[i] 
+    button.addEventListener('click', removeCartItem = (event) => {
+        const buttonClicked = event.target ;
+        buttonClicked.parentElement.remove() ;
+        updateCartTotal();
+    })
+} ;
+
+
+const updateCartTotal = () => {
+     const cartContainer = document.getElementsByClassName('cart-items')[0] ;
+     const cartItem = cartContainer.getElementsByClassName('cart-item') ;
+     let total = 0 ;
+     for (i = 0; i < cartItem.length; i++) {
+        const row = cartItem[i] ;
+        const priceElement = row.getElementsByClassName('item-price')[0] ;
+        const quantityInput = row.getElementsByClassName("item-quantity")[0] ;
+        const price = parseFloat(priceElement.innerText.replace('R', '')) ;
+        const quantity = quantityInput.value ;
+        console.log(price*quantity) ;
+        total = total + (price*quantity) ;
+     }
+     document.getElementsByClassName('cart-total-amount')[0].innerText = 'R' + total ;
+}
